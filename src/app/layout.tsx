9be10f375cfc40e334b6dcaf0ba668f9133d6de0
JottaP2 <joao.marinho@arapiraca.ufal.br>
2025-04-14 12:54:05 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Kanit } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kanit = Kanit({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={kanit.className}>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ src/components/Section1/index.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Cards from "../Cards";
+
+function Section1() {
+    return (
+        <>
+            <main className="bg-zinc-950 w-full min-h-screen flex flex-col items-center text-white text-center px-4 py-8 md:p-16 lg:p-30 overflow-hidden">
+                <Image
+                    src="/img/Jotta.PNG"
+                    alt="Picture of the author"
+                    width={200}
+                    height={200}
+                    className="rounded-full mb-5 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-[200px] lg:h-[200px] object-cover"
+                />
+                <h1 className="text-3xl md:text-4xl font-medium">Olá, sou o jotta!</h1>
+                <h2 className="text-center max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl font-light mt-4 text-sm sm:text-base md:text-lg">
+                    tenho 20 anos e sou estudante de Sistemas de Informação pela UFAL. Sou apaixonado por tecnologia e,
+                    atualmente, estou focado em aprofundar meus conhecimentos em programação front-end, pesquisa
+                    acadêmica e desenvolvimento de projetos ligados à minha formação.
+                </h2>
+
+                <div className="w-full md:w-250 mt-8 md:mt-5 flex flex-col items-center">
+                    <div className="flex flex-col md:flex-row w-full gap-6 md:gap-8 overflow-visible">
+                        <div className="w-full md:w-1/2">
+                            <Cards
+                                title="Tech Stack"
+                                technologies={[
+                                    { name: "Next.js", image: "/img/nextjs.svg" },
+                                    { name: "React", image: "/img/react.png" },
+                                    { name: "JavaScript", image: "/img/js.png" },
+                                    { name: "HTML", image: "/img/html.png" },
+                                    { name: "CSS", image: "/img/css.png" }
+                                ]}
+                            />
+                        </div>
+                        <div className="w-full md:w-1/2">
+                            <Cards
+                                title="Tools"
+                                technologies={[
+                                    { name: "Next.js", image: "/img/nextjs.svg" },
+                                    { name: "React", image: "/img/react.png" },
+                                    { name: "JavaScript", image: "/img/js.png" },
+                                    { name: "HTML", image: "/img/html.png" },
+                                    { name: "CSS", image: "/img/css.png" }
+                                ]} 
+                            />
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer className="bg-zinc-950 flex items-center justify-center text-white text-center p-2">
+                <h1 className="">jotta © 2025 - Todos os direitos reservados.</h1>
+            </footer>
+        </>
+    );
+}
+
+export default Section1;
